6b47b160007eb75c53ed01107c0dd036fc5f857d
Lilly:3 <72206869+LillyProgrammingIncorprated@users.noreply.github.com>
2020-10-01 18:37:55 +0300

    Update NewProject.cpp

--- NewProject.cpp
+++ NewProject.cpp
@@ -123,13 +123,14 @@ void PrintGrid()
   std::cout << std::endl;
 }
 
-void EditGrid()
+int EditGrid()
 {
   std::cout << "CURRENT GRID: " << std::endl;
   PrintGrid();
   int x, y, a, b, c;
   std::cout << "If you want to modify the grid, input 1. If you want to continue with the current grid, input 2:  ";
   std::cin >> a;
+
 
   switch (a)
   {
@@ -138,7 +139,7 @@ void EditGrid()
       while (a == 1)
       {
         int r;
-        std::cout << "Input 1 for manual editing. Input 2 to make a line that goes up from the beginning. Input 3 for a line that goes down. Input 4 for a line that goes right. Input 5 for a line that goes right. Input 6 for a diagonal right line Input 7 for a diagonal left line:  ";
+        std::cout << "Input 1 for manual editing. Input 2 to make a line that goes up from the beginning. Input 3 for a line that goes down. Input 4 for a line that goes right. Input 5 for a line that goes right. Input 6 for a diagonal right line. Input 7 for a diagonal left line:  ";
         std::cin >> r;
         switch (r)
         {
@@ -146,14 +147,43 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the cell you want to change:  ";
             std::cin >> x;
-            std::cout << "Input the y coordinate of the cell you want to change:   ";
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl;
+              break;
+            }
+            std::cout << "Input the y coordinate of the cell you want to change:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl;
+              break;
+            }
 
             std::cout << "Input the binary value you want to give to the cell:  ";
             std::cin >> b;
-            Cells[y][x] = b;
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+              switch (b)
+              {
+                case 1:
+                {
+                  Cells[y][x] = b;
+                  std::cout << "NEW GRID: " << std::endl << std::endl;
+                  PrintGrid();
+                  break;
+                }
+                case 0:
+                {
+                  Cells[y][x] = b;
+                  std::cout << "NEW GRID: " << std::endl << std::endl;
+                  PrintGrid();
+                  break;
+                }
+                default:
+                {
+                  std::cout << "Invalid input" << std::endl << std::endl;
+                  break;
+                }
+              }
             break;
           }
 
@@ -161,16 +191,45 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawUpLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawUpLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawUpLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
           }
 
@@ -178,16 +237,45 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawDownLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawDownLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawDownLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
           }
 
@@ -195,16 +283,45 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawRightLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawRightLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawRightLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
           }
 
@@ -212,16 +329,45 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawLeftLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawLeftLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawLeftLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
           }
 
@@ -229,51 +375,138 @@ void EditGrid()
           {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawDiagonalRightLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawDiagonalRightLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawDiagonalRightLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
-          }
+            }
+
             case 7:
-          {
+            {
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> x;
+            if (x > xDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the x coordinate of the beginning of the line:  ";
             std::cin >> y;
+            if (y > yDimension)
+            {
+              std::cout << "Invalid Input!" << std::endl << std::endl;
+              break;
+            }
             std::cout << "Input the lenght of the line you want: ";
             std::cin >> c;
 
             std::cout << "Input the binary value you want to give to the line:  ";
             std::cin >> b;
-            drawDiagonalLeftLine(x, y, c, b);
-            std::cout << "NEW GRID: " << std::endl;
-            PrintGrid();
+            switch (b)
+            {
+              case 1:
+              {
+                drawDiagonalLeftLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              case 0:
+              {
+                drawDiagonalLeftLine(x, y, c, b);
+                std::cout << "NEW GRID: " << std::endl << std::endl;
+                PrintGrid();
+                break;
+              }
+              default:
+              {
+                std::cout << "Invalid input" << std::endl << std::endl;
+                break;
+              }
+            }
             break;
+          }
+          default:
+          {
+            std::cout << "Invalid Input"  << std::endl << std::endl;
           }
         }
         std::cout << "If you want to change another cell, input 1. If you want to carry on with the current grid, input 2: ";
         std::cin >> a;
+        if (a == 1 || a == 2)
+        {
+          std::cout << std::endl << std::endl;
+        }
+        else
+        {
+          std::cout << "Invalid Input" << std::endl << std::endl;
+          break;
+        }
       }
       break;
+      return 0;
     }
-      case 2:
+    case 2:
     {
       break;
+      return 0;
+    }
+    default:
+    {
+      std::cout << "Invalid Input" << std::endl << std::endl;
+      return 1;
     }
   }
+  return 0;
 }
 
 
 int main()
 {
-  EditGrid();
+  auto EditGridF = EditGrid();
+
+  if (EditGridF == 1)
+  {
+    return 0;
+  }
+  else
+  {}
 
   for(int i = 0; i < 100; ++i)
   {
